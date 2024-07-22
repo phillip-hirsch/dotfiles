484e40331ac30a509fc19fb3853993bdb65b9940
phillip-hirsch <phillip@350home.com>
2024-07-22 11:33:06 -0400

    feat: Add nvim configuration files

---
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        html = { { "prettierd", "prettier" } },
+        css = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        markdown = { { "prettierd", "prettier" } },
+      },
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
